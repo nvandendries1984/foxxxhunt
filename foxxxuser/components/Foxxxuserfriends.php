89548e231f40e0fffed24501bd2f8c2ceac68069
NielsvandenDries <niels@openmindedit.nl>
2023-10-20 13:45:32 +0200

    update

---
+++ foxxxuser/components/Foxxxuserfriends.php
@@ -0,0 +1,19 @@
+<?php namespace Foxxxhunt\Foxxxuser\Components;
+
+use Cms\Classes\ComponentBase;
+
+class Foxxxuserfriends extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name' => 'Foxxxuserfriends Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+}

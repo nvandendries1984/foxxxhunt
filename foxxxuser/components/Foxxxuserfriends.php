048c3be448620ef0174395e050d847410e688dc5
NielsvandenDries <niels@openmindedit.nl>
2023-10-20 16:36:30 +0200

    update

--- foxxxuser/components/Foxxxuserfriends.php
+++ foxxxuser/components/Foxxxuserfriends.php
@@ -1,6 +1,9 @@
 <?php namespace Foxxxhunt\Foxxxuser\Components;
 
 use Cms\Classes\ComponentBase;
+use Foxxxhunt\Foxxxuser\Models\UserModel;
+use RainLab\User\Models\User;
+use Auth;
 
 class Foxxxuserfriends extends ComponentBase
 {
